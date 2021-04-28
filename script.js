82ab7f11a3343be75344a785370598c337f3e24e
José Trovarelli Neto <jtneto1995@hotmail.com>
2021-04-28 16:20:18 -0300

    funcao de comer e crescer

--- script.js
+++ script.js
@@ -45,6 +45,7 @@ function iniciarJogo(){
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box
 
+    
     criarBG()
     criarCobrinha()
     drawFood()
@@ -52,12 +53,19 @@ function iniciarJogo(){
     let snakeX = snake[0].x
     let snakeY = snake[0].y
 
+    
     if(direction == "right") snakeX += box
     if(direction == "left") snakeX -= box
     if(direction == "up") snakeY -= box
     if(direction == "down") snakeY += box
 
-    snake.pop()
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop()
+    }else{
+        food.x = Math.floor(Math.random() * 15 + 1) * box
+        food.y = Math.floor(Math.random() * 15 + 1) * box
+    }
+
 
     let newHead = {
         x: snakeX,
